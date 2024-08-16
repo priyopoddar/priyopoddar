b9c45e2b28249f5b7a9e837b73cbc82402977230
Priyo poddar <priyopoddar510@gmail.com>
2024-08-16 15:48:13 +0600

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed((".skills"),{
+    strings : ["Frontend Developer","Youtuber","Ui Desinger"],
+    typeSpeed :100,
+    backSpeed :100,
+    backDelay :1000,
+    loop : true
+});
